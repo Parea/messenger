3dd88958d888175aa74eb5327993e8131cb8f4ec
Parea <taie.taataparea1@gmail.com>
2018-01-12 13:47:41 -1000

    first commit

---
+++ messenger/database/seeds/MessagesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MessagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    for($i = 0; $i < 10; $i++):
+        $userID = rand(1, 10);
+        $validated = rand(0, 2);
+        $validatedBy = ($validated == 0)?0:1;
+        
+        $messages[] = [
+            'user_id'=> $userID,
+            'message' => str_random(10),
+            'validated' => $validated,
+            'validated_by' => $validatedBy,];
+    endfor;
+
+    foreach($messages AS $message):
+        DB::table('messages')->insert($message);
+    endforeach;
+
+    }
+}
